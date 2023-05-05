6757abeb73845e164be518a8ad196b56b3499e57
Raphael Henrique <raphaelrochaacft@gmail.com>
2023-05-05 13:58:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SorteioQuermesse
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int participante, numParticipante, numeroSorteio;
+
+            Random random = new Random();
+
+            Console.Write("Digite o numero de participantes da festa: ");
+            participante = int.Parse(Console.ReadLine());
+            
+            
+            for(numParticipante = 1; numParticipante <= participante ; numParticipante++) 
+            {
+                numeroSorteio = random.Next(1, participante);
+                Console.Write("\nNumero de entrada: " + numParticipante + " : " + numeroSorteio + "\n");
+                
+                if (numParticipante == numeroSorteio)
+                {
+                    Console.Write("(O numero vencedor foi: " + numParticipante + ")\n\n");
+                }
+            }
+        }
+    }
+}
